29bf9b0df547af8aeb2c029cbbc8fb77717b92bf
xtophe xanon <xocon10@gmail.com>
2020-09-01 21:07:48 -0500

    still not working

--- controllers/apiRoutes/post-routes.js
+++ controllers/apiRoutes/post-routes.js
@@ -53,46 +53,45 @@ router.put('/:id', withAuth, imgUpload.single('work-img'), (req, res) => {
 			cloudinary.uploader.destroy(oldPublicId.public_id, () => {
 				console.log(oldPublicId, ' deleted');
 			});
-		})
-		.catch((err) => {
-			console.log(err);
-			res.status(500).json(err);
-		});
-
-	Post.update(
-		{
-			title       : req.body.title,
-			dimension   : req.body.dimensions,
-			description : req.body.description,
-			media       : req.body.media,
-			img_url     : req.file.path,
-			public_id   : req.file.filename
-		},
-		{
-			where : {
-				id : req.params.id
-			}
-		}
-	)
-		.then((newPostData) => {
-			if (!newPostData) {
-				console.log('no new post data');
-				res.status(404).json({ message: 'No post found with this id' });
-				return;
-			}
-			// console.log('updated data:', newPostData);
-			// const newData = newPostData.get({ plain: true });
-			// console.log('updated public_id: ', newData.public_id);
-			// console.log('updated title', newData.title);
-			req.flash('success', 'Your work has been updated!');
-			res.json(newPostData);
+			Post.update(
+				{
+					title       : req.body.title,
+					dimension   : req.body.dimensions,
+					description : req.body.description,
+					media       : req.body.media,
+					img_url     : req.file.path,
+					public_id   : req.file.filename
+				},
+				{
+					where : {
+						id : req.params.id
+					}
+				}
+			).then((newPostData) => {
+        console.log('updated data:', newPostData);
+				if (!newPostData) {
+          console.log('no new post data');
+        const newData = newPostData.get({ plain: true });
+				console.log('updated public_id: ', newData.public_id);
+				console.log('updated title', newData.title);
+					res.status(404).json({ message: 'No post found with this id' });
+					return;
+				}
+				// no logging
+				
+				// error...newPostData.get not a function
+				// const newData = newPostData.get({ plain: true });
+				// console.log('updated public_id: ', newData.public_id);
+				// console.log('updated title', newData.title);
+				req.flash('success', 'Your work has been updated!');
+				res.json(newPostData);
+			});
 		})
 		.catch((err) => {
 			console.log(err);
 			res.status(500).json(err);
 		});
 });
-
 
 // DELETE /api/posts/1
 router.delete('/:id', withAuth, (req, res) => {
