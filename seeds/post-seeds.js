9528484bc208abffb03620f6713f49fb23d2acf1
xtophe xanon <xocon10@gmail.com>
2020-09-04 10:39:46 -0500

    customize seed templates to project models

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -3,103 +3,183 @@ const { Post } = require('../models');
 const postdata = [
   {
     title: 'Donec posuere metus vitae ipsum.',
-    post_url: 'https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png',
+    artist_name: '',
+    dimension: '',
+    description: '',
+    media: '',
+    img_url: '',
     user_id: 10
   },
   {
     title: 'Morbi non quam nec dui luctus rutrum.',
-    post_url: 'https://nasa.gov/donec.json',
+    artist_name: '',
+    dimension: '',
+    description: '',
+    media: '',
+    img_url: '',
     user_id: 8
   },
   {
     title: 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue.',
-    post_url: 'https://europa.eu/parturient/montes/nascetur/ridiculus/mus/etiam/vel.aspx',
+    artist_name: '',
+    dimension: '',
+    description: '',
+    media: '',
+    img_url: '',
     user_id: 1
   },
   {
     title: 'Nunc purus.',
-    post_url: 'http://desdev.cn/enim/blandit/mi.jpg',
+    artist_name: '',
+    dimension: '',
+    description: '',
+    media: '',
+    img_url: '',
     user_id: 4
   },
   {
     title: 'Pellentesque eget nunc.',
-    post_url: 'http://google.ca/nam/nulla/integer.aspx',
+    artist_name: '',
+    dimension: '',
+    description: '',
+    media: '',
+    img_url: '',
     user_id: 7
   },
   {
     title: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
-    post_url: 'https://stanford.edu/consequat.png',
+    artist_name: '',
+    dimension: '',
+    description: '',
+    media: '',
+    img_url: '',
     user_id: 4
   },
   {
     title: 'In hac habitasse platea dictumst.',
-    post_url: 'http://edublogs.org/non/ligula/pellentesque.js',
+    artist_name: '',
+    dimension: '',
+    description: '',
+    media: '',
+    img_url: '',
     user_id: 1
   },
   {
     title: 'Morbi non quam nec dui luctus rutrum.',
-    post_url: 'http://ucla.edu/consequat/nulla.html',
+    artist_name: '',
+    dimension: '',
+    description: '',
+    media: '',
+    img_url: '',
     user_id: 1
   },
   {
     title: 'Duis ac nibh.',
-    post_url: 'http://theguardian.com/dui/vel/nisl/duis/ac/nibh.aspx',
+    artist_name: '',
+    dimension: '',
+    description: '',
+    media: '',
+    img_url: '',
     user_id: 9
   },
   {
     title: 'Curabitur at ipsum ac tellus semper interdum.',
-    post_url: 'https://reverbnation.com/ligula/sit.jpg',
+    artist_name: '',
+    dimension: '',
+    description: '',
+    media: '',
+    img_url: '',
     user_id: 5
   },
   {
     title: 'In hac habitasse platea dictumst.',
-    post_url: 'http://china.com.cn/lectus/vestibulum.json',
+    artist_name: '',
+    dimension: '',
+    description: '',
+    media: '',
+    img_url: '',
     user_id: 3
   },
   {
     title: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.',
-    post_url: 'http://networksolutions.com/nam/ultrices/libero/non/mattis/pulvinar.json',
+    artist_name: '',
+    dimension: '',
+    description: '',
+    media: '',
+    img_url: '',
     user_id: 10
   },
   {
     title: 'Donec dapibus.',
-    post_url: 'https://instagram.com/ac/neque/duis/bibendum/morbi/non.xml',
+    artist_name: '',
+    dimension: '',
+    description: '',
+    media: '',
+    img_url: '',
     user_id: 8
   },
   {
     title: 'Nulla tellus.',
-    post_url: 'https://lycos.com/natoque/penatibus/et.html',
+    artist_name: '',
+    dimension: '',
+    description: '',
+    media: '',
+    img_url: '',
     user_id: 3
   },
   {
     title: 'Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo.',
-    post_url: 'https://gmpg.org/lorem.jpg',
+    artist_name: '',
+    dimension: '',
+    description: '',
+    media: '',
+    img_url: '',
     user_id: 3
   },
   {
     title:
       'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam.',
-    post_url: 'https://paginegialle.it/mattis/egestas.jsp',
+      artist_name: '',
+      dimension: '',
+      description: '',
+      media: '',
+      img_url: '',
     user_id: 7
   },
   {
     title: 'In hac habitasse platea dictumst.',
-    post_url: 'http://wikia.com/turpis/eget.jpg',
+    artist_name: '',
+    dimension: '',
+    description: '',
+    media: '',
+    img_url: '',
     user_id: 6
   },
   {
     title: 'Etiam justo.',
-    post_url: 'https://shareasale.com/quis.json',
+    artist_name: '',
+    dimension: '',
+    description: '',
+    media: '',
+    img_url: '',
     user_id: 4
   },
   {
     title: 'Nulla ut erat id mauris vulputate elementum.',
-    post_url: 'http://java.com/diam/neque/vestibulum/eget/vulputate/ut/ultrices.png',
+    artist_name: '',
+    dimension: '',
+    description: '',
+    media: '',
+    img_url: '',
     user_id: 6
   },
   {
     title: 'Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.',
-    post_url: 'https://java.com/at/nibh/in.png',
+    artist_name: '',
+    dimension: '',
+    description: '',
+    media: '',
+    img_url: '',
     user_id: 7
   }
 ];
--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -4,52 +4,92 @@ const userdata = [
   {
     username: 'alesmonde0',
     email: 'nwestnedge0@cbc.ca',
-    password: 'password123'
+    password: 'password123',
+    bio: '',
+    medium: '',
+    interests: '',
+    user_img_url: ''
   },
   {
     username: 'jwilloughway1',
     email: 'rmebes1@sogou.com',
-    password: 'password123'
+    password: 'password123',
+    bio: '',
+    medium: '',
+    interests: '',
+    user_img_url: ''
   },
   {
     username: 'iboddam2',
     email: 'cstoneman2@last.fm',
-    password: 'password123'
+    password: 'password123',
+    bio: '',
+    medium: '',
+    interests: '',
+    user_img_url: ''
   },
   {
     username: 'dstanmer3',
     email: 'ihellier3@goo.ne.jp',
-    password: 'password123'
+    password: 'password123',
+    bio: '',
+    medium: '',
+    interests: '',
+    user_img_url: ''
   },
   {
     username: 'djiri4',
     email: 'gmidgley4@weather.com',
-    password: 'password123'
+    password: 'password123',
+    bio: '',
+    medium: '',
+    interests: '',
+    user_img_url: ''
   },
   {
     username: 'msprague5',
     email: 'larnout5@imdb.com',
-    password: 'password123'
+    password: 'password123',
+    bio: '',
+    medium: '',
+    interests: '',
+    user_img_url: ''
   },
   {
     username: 'mpergens6',
     email: 'hnapleton6@feedburner.com',
-    password: 'password123'
+    password: 'password123',
+    bio: '',
+    medium: '',
+    interests: '',
+    user_img_url: ''
   },
   {
     username: 'tpenniell7',
     email: 'kperigo7@china.com.cn',
-    password: 'password123'
+    password: 'password123',
+    bio: '',
+    medium: '',
+    interests: '',
+    user_img_url: ''
   },
   {
     username: 'msabbins8',
     email: 'lmongain8@google.ru',
-    password: 'password123'
+    password: 'password123',
+    bio: '',
+    medium: '',
+    interests: '',
+    user_img_url: ''
   },
   {
     username: 'jmacarthur9',
     email: 'bsteen9@epa.gov',
-    password: 'password123'
+    password: 'password123',
+    bio: '',
+    medium: '',
+    interests: '',
+    user_img_url: ''
   }
 ];
 
