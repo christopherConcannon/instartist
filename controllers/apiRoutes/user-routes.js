f1c2a6c0c5c7ac5615e97e5138f3d04fed1d163b
xtophe xanon <xocon10@gmail.com>
2020-09-04 22:12:32 -0500

    fix fk issue on user delete

--- controllers/apiRoutes/user-routes.js
+++ controllers/apiRoutes/user-routes.js
@@ -4,59 +4,59 @@ const withAuth = require('../../utils/auth');
 const imgUpload = require('../../config/imgUpload');
 
 // GET /api/users
-router.get('/', (req, res) => {
-	User.findAll({
-		attributes : { exclude: [ 'password' ] }
-	})
-		.then((dbUserData) => res.json(dbUserData))
-		.catch((err) => {
-			console.log(err);
-			res.status(500).json(err);
-		});
-});
+// router.get('/', (req, res) => {
+// 	User.findAll({
+// 		attributes : { exclude: [ 'password' ] }
+// 	})
+// 		.then((dbUserData) => res.json(dbUserData))
+// 		.catch((err) => {
+// 			console.log(err);
+// 			res.status(500).json(err);
+// 		});
+// });
 
 // GET /api/users/1
-router.get('/:id', (req, res) => {
-	User.findOne({
-		attributes : { exclude: [ 'password' ] },
-		where      : {
-			id : req.params.id
-		},
-		include    : [
-			{
-				model      : Post,
-				attributes : [
-					'id',
-					'title',
-					'dimension',
-					'description',
-					'media',
-					'img_url',
-					'created_at'
-				]
-			},
-			{
-				model      : Comment,
-				attributes : [ 'id', 'comment_text', 'created_at' ],
-				include    : {
-					model      : Post,
-					attributes : [ 'title' ]
-				}
-			}
-		]
-	})
-		.then((dbUserData) => {
-			if (!dbUserData) {
-				res.status(404).json({ message: 'No user found with this id' });
-				return;
-			}
-			res.json(dbUserData);
-		})
-		.catch((err) => {
-			console.log(err);
-			res.status(500).json(err);
-		});
-});
+// router.get('/:id', (req, res) => {
+// 	User.findOne({
+// 		attributes : { exclude: [ 'password' ] },
+// 		where      : {
+// 			id : req.params.id
+// 		},
+// 		include    : [
+// 			{
+// 				model      : Post,
+// 				attributes : [
+// 					'id',
+// 					'title',
+// 					'dimension',
+// 					'description',
+// 					'media',
+// 					'img_url',
+// 					'created_at'
+// 				]
+// 			},
+// 			{
+// 				model      : Comment,
+// 				attributes : [ 'id', 'comment_text', 'created_at' ],
+// 				include    : {
+// 					model      : Post,
+// 					attributes : [ 'title' ]
+// 				}
+// 			}
+// 		]
+// 	})
+// 		.then((dbUserData) => {
+// 			if (!dbUserData) {
+// 				res.status(404).json({ message: 'No user found with this id' });
+// 				return;
+// 			}
+// 			res.json(dbUserData);
+// 		})
+// 		.catch((err) => {
+// 			console.log(err);
+// 			res.status(500).json(err);
+// 		});
+// });
 
 // POST /api/users -- create user on signup
 router.post('/', imgUpload.single('user-img'), (req, res) => {
