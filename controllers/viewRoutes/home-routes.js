53afa73df3c60b09f919925e4c747361b7586f7f
xtophe xanon <xocon10@gmail.com>
2020-09-05 13:59:05 -0500

    update comment styles etc

--- controllers/viewRoutes/home-routes.js
+++ controllers/viewRoutes/home-routes.js
@@ -69,7 +69,7 @@ router.get('/post/:id', (req, res) => {
 				attributes : [ 'id', 'comment_text', 'post_id', 'user_id', 'created_at' ],
 				include    : {
 					model      : User,
-					attributes : [ 'username' ]
+					attributes : [ 'username', 'user_img_url' ]
 				}
 			},
 			{
