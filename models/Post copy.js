6fc531ffc51ab7b7c0fafd734dd3dfe95c8b5767
xtophe xanon <xocon10@gmail.com>
2020-09-05 21:46:31 -0500

    copy post model and controller before merge

---
+++ models/Post copy.js
@@ -0,0 +1,58 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model { }
+
+Post.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true
+		},
+		title: {
+			type: DataTypes.STRING,
+			allowNull: false
+    },
+		dimension: {
+      type: DataTypes.STRING,
+			allowNull: false
+		},
+		description: {
+      type: DataTypes.STRING,
+			allowNull: false
+		},
+		media: {
+      type: DataTypes.STRING,
+			allowNull: false
+		},
+    img_url: { // isUrl: true,   ver q va a guardar
+      type: DataTypes.STRING,
+      allowNull: false,
+			validate: {
+				isUrl: true
+			}
+    },
+    // file name of image in Cloudinary, needed for deleting
+    public_id: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+		user_id: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: 'user',
+				key: 'id'
+			}
+		}
+	},
+	{
+		sequelize,
+		freezeTableName: true,
+		underscored: true,
+		modelName: 'post'
+	}
+);
+
+module.exports = Post;
