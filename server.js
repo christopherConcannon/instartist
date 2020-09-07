88be25a9ef5d527fc39370010b9e81679c2c2677
xtophe xanon <xocon10@gmail.com>
2020-09-06 20:17:13 -0500

    add favicon.  restore mysteriously moved db directory

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const favicon = require('serve-favicon');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const app = express();
@@ -8,21 +9,21 @@ const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
 const flash = require('connect-flash');
-const session = require("express-session");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
-
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 // set up express session and connect session to Sequelize db
 const sess = {
-  secret: process.env.SESS_SECRET,
-  cookie: {maxAge: 3600000},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
+	secret            : process.env.SESS_SECRET,
+	cookie            : { maxAge: 3600000 },
+	resave            : false,
+	saveUninitialized : true,
+	store             : new SequelizeStore({
+		db : sequelize
+	})
 };
 
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(session(sess));
 
 app.use(express.json());
@@ -32,13 +33,13 @@ app.use(flash());
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use((req, res, next) => {
-  res.locals.error =  req.flash('error');
-  res.locals.success = req.flash('success');
-  next();
-})
+	res.locals.error = req.flash('error');
+	res.locals.success = req.flash('success');
+	next();
+});
 app.use(routes);
 
 // sequelize.sync({ force: true }).then(() => {
-sequelize.sync({ force: false}).then(() => {
-  app.listen(PORT, () => console.log(`Now listening on PORT ${PORT}`));
+sequelize.sync({ force: false }).then(() => {
+	app.listen(PORT, () => console.log(`Now listening on PORT ${PORT}`));
 });
